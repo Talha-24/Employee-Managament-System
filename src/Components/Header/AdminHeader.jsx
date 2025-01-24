215f83d8da60dffed635cc51afe44ffc79c628ac
Mohammad Talha <talhabhatti82542@gmail.com>
2025-01-24 11:05:47 +0500

    Add Responsiveness to Admin Header

--- src/Components/Header/AdminHeader.jsx
+++ src/Components/Header/AdminHeader.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import { DataSender } from '../Context API/AuthContext'
+import "./Header.css";
 
 const AdminHeader = (propse) => {
    
@@ -23,8 +24,8 @@ const AdminHeader = (propse) => {
     
     
   return (
-    <div className='p-10 bg-[#1C1C1C] flex items-end justify-between border-[1px] border-purple-600 '>
-        <h1 id="adminh1"  className='text-2xl text-white font-semibold'>Hello <br /> <span className='text-4xl text-white font-bold'>Talha(CEO)!👋</span></h1>
+    <div className='p-[4vw] bg-[#1C1C1C] flex items-end justify-between border-[1px] border-purple-600 '>
+        <h1 id="adminh1"  className='text-[3vmin] text-white font-semibold'>Hello <br /> <span className='text-[5vmin] text-white font-bold'>Talha(CEO)!👋</span></h1>
         <button id="adminbtn" onClick={()=>{Logoutbtn()}}   className='bg-purple-600 py-3 px-4 text-xl font-medium text-white rounded-sm active:scale-[0.5] active:bg-red-600'>Log Out</button>
     </div>
   )
